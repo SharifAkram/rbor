d37fe5f187394104a98435e9cb0098daf56d6bb5
Sharif Akram <rifkit@gmail.com>
2023-10-08 00:28:44 -0400

    add code samples from Ch1

---
+++ 01/countDownAndUp.py
@@ -0,0 +1,14 @@
+def countDownAndUp(number):
+    print(number)
+    if number == 0:
+        # Base Case
+        print("Reached the base case.")
+        return
+    else:
+        # Recursive Case
+        countDownAndUp(number - 1)
+        print(number, "returning")
+        return
+
+
+countDownAndUp(3)
---
+++ 01/shortestWithBaseCase.py
@@ -0,0 +1,18 @@
+def shortestWithBaseCase(makeRecursiveCall):
+    print("shortestWithBaseCase(%s) called." % makeRecursiveCall)
+    if not makeRecursiveCall:
+        # Base Case
+        print("Returning from base case.")
+        return
+    else:
+        # Recursive Case
+        shortestWithBaseCase(False)
+        print("Returning from recursive case.")
+        return
+
+
+print("Calling shortestWithBaseCase(False):")
+shortestWithBaseCase(False)
+print()
+print("Calling shortestWithBase(True):")
+shortestWithBaseCase(True)

c721cc094b78e3394594aff779823cffe5b962fc
Sharif Akram <rifkit@gmail.com>
2023-12-10 11:01:40 -0500

    add code samples from Ch2

---
+++ 02/factorialByIteration.py
@@ -0,0 +1,11 @@
+# iterative factorial algorithm
+
+
+def factorial(number):
+    product = 1
+    for i in range(1, number + 1):
+        product = product * i
+    return product
+
+
+print(factorial(10))

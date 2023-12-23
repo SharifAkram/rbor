a367efdbef721bf80b08d8500f1c3dd661d7d22c
Sharif Akram <rifkit@gmail.com>
2023-12-23 12:02:07 -0500

    add code samples from Ch2

---
+++ 02/exponentByRecursion.py
@@ -0,0 +1,14 @@
+def exponentByRecursion(a, n):
+    if n == 1:
+        return a
+    elif n % 2 == 0:
+        result = exponentByRecursion(a, n // 2)
+        return result * result
+    elif n % 2 == 1:
+        result = exponentByRecursion(a, n // 2)
+        return result * result * a
+
+
+print(exponentByRecursion(3, 6))
+print(exponentByRecursion(10, 3))
+print(exponentByRecursion(17, 10))
